7a81d011ef1b823e551228332c4994889965503d
Jonathan Rajotte <jonathan.r.julien@gmail.com>
2014-03-06 11:13:53 -0500

    clean up

--- src/bin/lttng/mi.c
+++ src/bin/lttng/mi.c
@@ -62,19 +62,17 @@ const char * const mi_element_list_ust_jul_events               = "jul_events";
 const char * const mi_element_list_ust_curr_pid                 = "curr_pid";
 const char * const mi_element_list_ust_cmdline                  = "cmdline";
 
-LTTNG_HIDDEN
 mi_writer *mi_writer_create(int fd_output)
 {
+	//TODO JRJ: handle err ... with tmp file
     return config_writer_create(fd_output);
 }
 
-LTTNG_HIDDEN
 int mi_writer_destroy(mi_writer *writer)
 {
     return config_writer_destroy(writer);
 }
 
-LTTNG_HIDDEN
 int mi_writer_command_open(mi_writer *writer, const char* command)
 {
     int ret;
@@ -87,47 +85,40 @@ end:
     return ret;
 }
 
-LTTNG_HIDDEN
 int mi_writer_command_close(mi_writer *writer)
 {
     return mi_writer_close_element(writer);
 }
 
-LTTNG_HIDDEN
 int mi_writer_open_element(mi_writer *writer,
 		const char *element_name)
 {
     return config_writer_open_element(writer, element_name);
 }
 
-LTTNG_HIDDEN
 int mi_writer_close_element(mi_writer *writer)
 {
     return config_writer_close_element(writer);
 }
 
-LTTNG_HIDDEN
 int mi_writer_write_element_unsigned_int(mi_writer *writer,
 		const char *element_name, uint64_t value)
 {
     return config_writer_write_element_unsigned_int(writer, element_name, value);
 }
 
-LTTNG_HIDDEN
 int mi_writer_write_element_signed_int(mi_writer *writer,
 		const char *element_name, int64_t value)
 {
     return config_writer_write_element_signed_int(writer, element_name, value);
 }
 
-LTTNG_HIDDEN
 int mi_writer_write_element_bool(mi_writer *writer,
 		const char *element_name, int value)
 {
     return config_writer_write_element_bool(writer, element_name, value);
 }
 
-LTTNG_HIDDEN
 int mi_writer_write_element_string(mi_writer *writer,
 		const char *element_name, const char *value)
 {
